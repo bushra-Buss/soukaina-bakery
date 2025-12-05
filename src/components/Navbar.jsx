d2630a83574f1ef0e819fbe330ad5e8f641941ac
Ahmad Kadri <ahmadkadri.87@hotmail.com>
2025-12-05 15:39:54 +0100

    mobile scale

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -86,7 +86,7 @@ export default function Navbar() {
               letterSpacing: "1px",
             }}
           >
-            MyStore
+            Soukaina bakery
           </Typography>
 
           {/* Desktop Navigation */}
